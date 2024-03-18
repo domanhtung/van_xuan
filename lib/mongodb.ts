aa999829462f8e0acb9dc1bbaa7e4deac06d6040
domanhtung <51911941+domanhtung@users.noreply.github.com>
2024-03-18 17:38:23 +0700

    fix: file mongodb cannot compiled under

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -37,3 +37,5 @@
 // // Export a module-scoped MongoClient promise. By doing this in a
 // // separate module, the client can be shared across functions.
 // export default clientPromise;
+
+export default {};
